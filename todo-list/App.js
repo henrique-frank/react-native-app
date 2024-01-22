382ca0b939048e49231860a1e67290c6391d053c
Henrique Frank <61198260+henrique-frank@users.noreply.github.com>
2024-01-22 09:29:16 -0300

    Adding to app buttons and titles for testing

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text , StyleSheet, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HELLO WORLD</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  const [nome, setNome] = useState('Jorge')
+
+  function handleMudaNome(){
+   setNome('Aluno Jorge Henrique')
+  }
+
+  return(
+    <View style={ styles.container }>
+      <Text style={ styles.title }>Código da Aprovação</Text>
+      <Text style={ [styles.title, styles.text] }>Henrique Frank</Text>
+      <Text>{nome}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={handleMudaNome}>
+        <Text style={styles.buttonText}>Mudar nome</Text>
+      </TouchableOpacity>
     </View>
-  );
+
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'#121212',
+    paddingTop: 28,
   },
-});
+  title:{
+    fontSize: 32,
+    color: '#f1f1f1',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  text:{
+    color:'blue'
+  },
+  button:{
+    backgroundColor:'green',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30
+  },
+  buttonText:{
+    color: '#FFF',
+    fontWeight: 'bold'
+
+  }
+})
